bc48a7280d0615215e9fc28147624b7e51ea42ec
Amit Dhamu <DHAMUA@cpwplc.com>
2018-08-08 15:10:08 +0100

    Update deps

--- bin/install-deps.sh
+++ bin/install-deps.sh
@@ -4,27 +4,82 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update && brew upgrade
 
+# Brew packages
+brew install git \
+             imagemagick \
+             node \
+             phpunit \
+             p7zip \
+             python3 \
+             ssh-copy-id \
+             wget \
+             youtube-dl
+
+# Apps
+brew cask install 1clipboard \
+                  daisydisk \
+                  docker \
+                  dropbox \
+                  firefox \
+                  google-chrome \
+                  google-backup-and-sync \
+                  handbrake \
+                  iterm2 \
+                  itsycal \
+                  kap \
+                  microsoft-office \
+                  music-manager \
+                  mysqlworkbench \
+                  namechanger \
+                  robo-3t \
+                  sequel-pro \
+                  sip \
+                  sketch \
+                  skype \
+                  skype-for-business \
+                  sourcetree \
+                  spectacle \
+                  stride \
+                  sublime-text \
+                  tunnelblick \
+                  vlc \
+                  vnc-viewer
+
+# QuickLook Plugins
+brew cask install qlcolorcode \
+                  qlstephen \
+                  qlmarkdown \
+                  quicklook-json \
+                  qlimagesize \
+                  webpquicklook \
+                  suspicious-package \
+                  quicklookase \
+                  qlvideo
+
+# NPM packages
+npm install -g bower \
+               diff-so-fancy \
+               eslint \
+               git-recent \
+               gulp \
+               localtunnel \
+               nativefier \
+               nodemon \
+               pm2 \
+               speed-test \
+               stylelint \
+               taskbook \
+               yarn
+
 # Composer
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
 
-# Node
-brew install node
-
-# Python 3
-brew install python3
-
-# NPM packages
-npm install -g yarn gulp bower diff-so-fancy nativefier git-recent localtunnel nodemon pm2 taskbook
-
-# Brew packages
-brew cask install google-chrome firefox iterm2 mysqlworkbench sublime-text skype hipchat tunnelblick vlc
-brew install wget p7zip ssh-copy-id youtube-dl
-
-# QuickLook Plugins
-brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlimagesize webpquicklook suspicious-package quicklookase qlvideo
-
 # Google Music Manager
 pip3 install gmusicapi-scripts
+
+# Cleanup
+brew cleanup --force
+rm -rf /Library/Caches/Homebrew/*

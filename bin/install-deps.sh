ef337596db4e72e2f117e1bd451e572c790ca3f0
Amit Dhamu <dev@amitd.co>
2018-12-05 12:19:52 +0000

    Maintenance

--- bin/install-deps.sh
+++ bin/install-deps.sh
@@ -69,10 +69,11 @@ npm install -g diff-so-fancy \
 
 # Composer
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '93b54496392c062774670ac18b134c3b3a95e5a5e5c8f1a9f115f203b75bf9a129d5daa8ba6a13e2cc8a1da0806388a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
+mv composer.phar /usr/local/bin/composer
 
 # Cleanup
-brew cleanup --force
+brew cleanup
 rm -rf /Library/Caches/Homebrew/*

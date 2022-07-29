29de82b5d8571688ee78aaaa4906521d83bf181a
Amit Dhamu <adhamu@dunelm.com>
2022-07-29 10:21:15 +0100

    Forgot global files to ignore

--- bin/make.sh
+++ bin/make.sh
@@ -12,6 +12,9 @@ git config --global user.email "$EMAIL_ADDRESS"
 git config --global pull.rebase false
 git config --global init.defaultBranch main
 
+echo '.DS_Store' >>~/.gitignore_global
+echo 'yarn-error.log' >>~/.gitignore_global
+
 git config --global core.excludesfile ~/.gitignore_global
 
 echo "✅ Git config"
